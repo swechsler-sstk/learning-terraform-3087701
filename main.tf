d83da9502c9e19ac64516b29be8be3861195c334
swechsler-sstk <126686511+swechsler-sstk@users.noreply.github.com>
2024-06-03 16:25:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  vpc = "vpc-0022395a602ab987d"
 
   tags = {
     Name = "HelloWorld"
